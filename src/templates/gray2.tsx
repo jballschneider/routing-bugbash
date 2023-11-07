121600af69c3788d87c9b3d3deececd9b0420644
Joseph Ballschneider <jballschneider@yext.com>
2023-11-07 15:16:09 -0500

    fix gray2

--- src/templates/gray2.tsx
+++ src/templates/gray2.tsx
@@ -3,6 +3,6 @@ import { templateForColor } from "../ColorBase";
 import {Color} from "../PageLayout"
 
 const template = templateForColor(Color.GRAY)
-export const getPath = () => `${template.getPath}-foo`;
+export const getPath = () => `${template.getPath()}-foo`;
 export const getHeadConfig = template.getHeadConfig;
 export default template.default;
